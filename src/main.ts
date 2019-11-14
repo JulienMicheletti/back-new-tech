215365a8833252d4a021c0eb87b5c48ebd0f3c7d
Julien Micheletti <julien.micheletti1@gmail.com>
2019-11-14 14:17:52 +0100

    Fichier de configuration

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,29 @@ import { Logger } from '@nestjs/common';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppConfig } from './interfaces/app-config.interface';
+import { SwaggerConfig } from './interfaces/swagger-config.interface';
+import * as Config from 'config';
 
-async function bootstrap() {
+async function bootstrap(config: AppConfig, swaggerConfig: SwaggerConfig) {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
 
   const options = new DocumentBuilder()
-    .setTitle('Documentaton API')
-    .setDescription(`Documentation de l'API du projet de technologies du web`)
-    .setVersion('1.0')
-    .addTag('web')
+    .setTitle(swaggerConfig.title)
+    .setDescription(swaggerConfig.description)
+    .setVersion(swaggerConfig.version)
+    .addTag(swaggerConfig.tag)
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('documentation', app, document);
   await app.listen(3000);
 
-  Logger.log(`Application served at http://localhost:3000`, 'bootstrap');
-  Logger.log(`Application documentation at http://localhost:3000/documentation`, 'bootstrap');
+  Logger.log(`Application served at http://${config.host}:${config.port}`, 'bootstrap');
+  Logger.log(`Application documentation at http://${config.host}:${config.port}/documentation`, 'bootstrap');
 }
 
-bootstrap();
+bootstrap(Config.get<AppConfig>('server'), Config.get<SwaggerConfig>('swagger'));
