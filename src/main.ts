d29b606fe3aca4f1e6681156117b2d4c366e9d70
Julien Micheletti <julien.micheletti1@gmail.com>
2019-11-14 14:05:58 +0100

    Initialisation de la documentation + ajout des dépendances

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('Documentaton API')
+    .setDescription(`Documentation de l'API du projet de technologies du web`)
+    .setVersion('1.0')
+    .addTag('web')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('documentation', app, document);
   await app.listen(3000);
+
   Logger.log(`Application served at http://localhost:3000`, 'bootstrap');
+  Logger.log(`Application documentation at http://localhost:3000/documentation`, 'bootstrap');
 }
 
 bootstrap();
